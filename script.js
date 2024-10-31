14c31b865f5911207280c11489b30034b835e612
Iderlan Oliveira <iderlanoliveira19@gmail.com>
2024-10-31 14:09:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,4 +14,26 @@ quemSomosButton.addEventListener('click', () => scrollToSection('quem-somos'));
 nossaMissaoButton.addEventListener('click', () => scrollToSection('nossa-missao'));
 nossoObjetivoButton.addEventListener('click', () => scrollToSection('nosso-objetivo'));
 
-console.log("Testando se ta ok");
+function redirecionarParaWhatsApp() {
+    let userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    let telefone = this.getAttribute("data-phone");
+    let mensagem = encodeURIComponent(this.getAttribute("data-mensagem"));
+
+    if (/android|ipad|iphone|ipod/.test(userAgent.toLowerCase())) {
+        window.location.href = "whatsapp://send?phone=" + telefone + "&text=" + mensagem;
+    } else {
+        window.open("https://web.whatsapp.com/send?phone=" + telefone + "&text=" + mensagem, "_blank");
+    }
+}
+
+// Adiciona o evento de clique aos botões do WhatsApp
+let buttons = document.querySelectorAll(".btn-whatsapp");
+buttons.forEach(function(button) {
+    button.addEventListener("click", redirecionarParaWhatsApp);
+});
+
+// Adiciona o evento de clique ao botão que rola para a seção "planos"
+document.getElementById('button').addEventListener('click', function() {
+    var elemento = document.getElementById('planos');
+    elemento.scrollIntoView({ behavior: 'smooth', block: 'start' });
+});
